93237f84e18a4600ae7c1fd13f6c43fd711b898e
Marc Mills <mdmills1990@gmail.com>
2019-03-05 16:31:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
+let input = document.querySelector('input');
+
 function retrieveEmployeeInformation(){
-  return document.value;
+  return input.value;
 }
