a84f0a934670646dcfc0ecc841b65104e6474e7a
Marc Mills <mdmills1990@gmail.com>
2019-03-05 16:42:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,5 +28,7 @@ function addNewLiOnClick(){
 function clearEmployeeListOnLinkClick(){
   let link = document.querySelector('a');
   let ul = document.querySelector('ul');
-  link.addEventListener('')
+  link.addEventListener('click', function(e){
+    
+  })
 }
