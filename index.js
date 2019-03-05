040f3858a15821903f7a9ce8ffab856b77f0101b
Marc Mills <mdmills1990@gmail.com>
2019-03-05 16:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function retrieveEmployeeInformation(){
 
 function addNewElementAsLi(){
   let employee = retrieveEmployeeInformation();
-  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `${employee})
+  document.querySelector('.employee-list').insertAdjacentHTML('beforeend', `<li>${employee}</li>`)
 }
